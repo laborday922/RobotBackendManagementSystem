fdb5af92ddef403db130985ed4620fd3fbe84d31
xwx <3096345073@qq.com>
2026-04-16 13:07:36 +0800

    修改测试报错，doc修改

--- taskmgt/src/main/java/com/ruoyi/taskmgt/controller/StepController.java
+++ taskmgt/src/main/java/com/ruoyi/taskmgt/controller/StepController.java
@@ -5,6 +5,7 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.utils.CloneFactory;
+import com.ruoyi.common.validation.NewGroup;
 import com.ruoyi.taskmgt.controller.dto.TaskStepDto;
 import com.ruoyi.taskmgt.domain.bo.TaskStep;
 import com.ruoyi.taskmgt.service.IStepService;
@@ -28,17 +29,17 @@ import java.util.stream.Collectors;
 public class StepController extends BaseController {
     @Autowired
     private IStepService stepService;
-//    @ApiOperation("创建任务步骤")
-//    @Log(title = "创建步骤", businessType = BusinessType.INSERT)
-//    @PostMapping("tasks/{id}/steps")
-//    public AjaxResult createTaskSteps(@PathVariable Long id,@Validated(value = NewGroup.class) @RequestBody List<TaskStepDto> dtos)
-//    {
-//        List<TaskStep> steps = dtos.stream()
-//                .map(dto -> CloneFactory.copy(new TaskStep(), dto))
-//                .collect(Collectors.toList());
-//        List<TaskStepVo> result = this.stepService.createSteps(id,steps);
-//        return success(result);
-//    }
+    @ApiOperation("创建任务步骤")
+    @Log(title = "创建步骤", businessType = BusinessType.INSERT)
+    @PostMapping("tasks/{id}/steps")
+    public AjaxResult createTaskSteps(@PathVariable Long id,@Validated(value = NewGroup.class) @RequestBody List<TaskStepDto> dtos)
+    {
+        List<TaskStep> steps = dtos.stream()
+                .map(dto -> CloneFactory.copy(new TaskStep(), dto))
+                .collect(Collectors.toList());
+        List<TaskStepVo> result = this.stepService.createSteps(id,steps);
+        return success(result);
+    }
 
     @ApiOperation("修改任务步骤")
     @Log(title = "修改步骤", businessType = BusinessType.UPDATE)
@@ -60,11 +61,11 @@ public class StepController extends BaseController {
         List<TaskStepVo> result = this.stepService.retrieveSteps(id);
         return success(result);
     }
-//    @ApiOperation("完成步骤")
-//    @Log(title = "完成步骤", businessType = BusinessType.UPDATE)
-//    @PutMapping("steps/{id}/complete")
-//    public AjaxResult completeStep(@PathVariable Long id) {
-//        stepService.completeStep(id);
-//        return success();
-//    }
+    @ApiOperation("完成步骤")
+    @Log(title = "完成步骤", businessType = BusinessType.UPDATE)
+    @PutMapping("steps/{id}/complete")
+    public AjaxResult completeStep(@PathVariable Long id) {
+        stepService.completeStep(id);
+        return success();
+    }
 }

e7b0ad742ed0df3947a042ced8f57a30d64c0bd9
wyj <1031220679@qq.com>
2026-08-16 19:02:04 +0800

    debug

--- taskmgt/src/main/java/com/ruoyi/taskmgt/websocket/TaskRobotWebSocketHandler.java
+++ taskmgt/src/main/java/com/ruoyi/taskmgt/websocket/TaskRobotWebSocketHandler.java
@@ -24,7 +24,7 @@ public class TaskRobotWebSocketHandler extends RobotWebSocketHandler {
     private final Map<String, CompletableFuture<RobotTaskResponse>> pendingResponses = new ConcurrentHashMap<>();
 
     public TaskRobotWebSocketHandler(IRobotsService robotService, ApplicationEventPublisher eventPublisher, IRobotPositionHistoryService robotPositionHistoryService) {
-        super(robotService, eventPublisher, robotPositionHistoryService);
+        super(robotService, robotPositionHistoryService, eventPublisher);
     }
 
     @Override

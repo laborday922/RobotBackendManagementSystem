e38c4742e794c2bd6306862e64643826047a1bcd
wyj <1031220679@qq.com>
2026-08-27 16:27:44 +0800

    fix:机器人在线情况前端显示效果

--- ruoyi-ui/src/views/data/dataDashboard/robotDataDashboard.vue
+++ ruoyi-ui/src/views/data/dataDashboard/robotDataDashboard.vue
@@ -418,7 +418,7 @@ export default {
         const onlineCounts = this.groups.map(g => g.onlineCount)
         this.robotGroupBarChart.setOption({
           tooltip: {trigger: "axis", axisPointer: {type: "shadow"}, formatter: '{b}<br/>在线数量: {c}'},
-          grid: {top: 20, bottom: 20, left: 70, right: 50, containLabel: true},
+          grid: {top: 20, bottom: 20, left: 50, right: 80, containLabel: true},
           xAxis: {
             type: "value",
             name: "在线数量",
